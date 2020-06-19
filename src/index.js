bf1783d1a0f6e075abf17ed251d79567c2de7353
nevelson <nevelson92@gmail.com>
2020-06-19 10:22:10 -0400

    First push

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Root from './root/Root'
+//change
+ReactDOM.render(<Root />, document.getElementById('root'))
