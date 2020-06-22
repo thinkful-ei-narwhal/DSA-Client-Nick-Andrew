7ca64aa515bdd257e2ae2bd7292f034b6f830103
nevelson <nevelson92@gmail.com>
2020-06-22 11:04:09 -0400

    Updated

---
+++ build/precache-manifest.70ccc9358a55b7d4a8fac034601adea4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dabd77150d34d0e0a6a51c6d22dee9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d8c869f9bb9fc19e7ae",
+    "url": "/static/css/main.2cbc7b24.chunk.css"
+  },
+  {
+    "revision": "d5aa5ff7d734322142e2",
+    "url": "/static/js/2.2e5dcac5.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.2e5dcac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d8c869f9bb9fc19e7ae",
+    "url": "/static/js/main.e42ca205.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  },
+  {
+    "revision": "7cad3d5b9f7a36a0a52bb4de2df1d8f4",
+    "url": "/static/media/dogs.7cad3d5b.jpg"
+  }
+]);
